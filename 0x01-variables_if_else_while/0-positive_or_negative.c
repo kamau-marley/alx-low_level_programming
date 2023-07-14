a341fb48ddfbb7d7b93a7d921de6ca82100ed595
kamau-marley <designsmarline@gmail.com>
2023-07-14 04:43:51 -0700

    iwork tu

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,16 +6,16 @@
  * Return : Alaways 0 (Success)
  */
 
-int main() {
-    srand(time(0));  // Initialize random seed
+int main(void) {
+    srand(time(0)):
 
-    int n = rand() % 201 - 100;  // Generate random number between -100 and 100
-
-    printf("The number: %d\n", n);
-
-    if (n > 0) {
-        printf("is positive\n");
-    } else if (n == 0) {
+   	n = rand() - RAND_MAX / 2:
+    if (n > 0) 
+    {
+    	    printf("is positive\n");
+    } 
+    	else if (n == 0) 	
+	{
         printf("is zero\n");
     } else {
         printf("is negative\n");
