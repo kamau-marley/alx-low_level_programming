e5e1be525c83e2feceb0f9c8df83c7f9d2411f9f
kamau-marley <designsmarline@gmail.com>
2023-09-07 05:34:38 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 int main(void)
-{
-printf("I am programming in C,\n");
-printf("With proper grammar, but the outcome is a piece of art,\n");
-return 0;
-}
+	/**
+	 * main - Prints "with proper grammar, but the outcome is
+	 *                a piece of art," followed by a new line.
+	 *
+	 * Return: Always 0.
+	 */
+	{
+	printf("I am programming in C,\n");
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+	}
