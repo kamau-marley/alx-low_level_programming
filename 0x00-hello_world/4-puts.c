db4a3ca73be9ef686a8890b74a5fbe7d6829ca74
kamau-marley <designsmarline@gmail.com>
2023-09-07 05:20:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
-/* I am Kamau. this script should put 'programming is like building a multilingual puzzle'
- *
- * */
+/* I am Kamau. this script put 'programming is like building a multilingual puzzl */
 
 #include <stdio.h>
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
