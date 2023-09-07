b7e483b090f0dabce7f895cee0ed3244eb5a3297
kamau-marley <designsmarline@gmail.com>
2023-09-07 05:27:10 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-/*q 4-puts.c */
-
 #include <stdio.h>
-int main(void)
-/*main - Prints "Programming is like building a multilingual
+
+/* main - Prints "Programming is like building a multilingual
  *                puzzle, followed by a new line.
  */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
