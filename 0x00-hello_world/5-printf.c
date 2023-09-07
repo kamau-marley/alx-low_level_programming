13f2fb291b960912191e94b95ef9cf20c4adf75c
kamau-marley <designsmarline@gmail.com>
2023-09-07 05:51:38 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 	 * main - Prints "with proper grammar, but the outcome is
 	 * a piece of art, followed by a new line."
 	 *
-	 * The function prints "with proper grammar, but the outcome is a piece of art,, followed by a new line."
+	 * The function prints "with proper grammar, but the outcome is a
+	 * piece of art,, followed by a new line."
 	 * Return: Always 0.
 	 */
 
