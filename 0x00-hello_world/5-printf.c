757bfc243fad30e2029de0f28859ba5a92508f8d
kamau-marley <designsmarline@gmail.com>
2023-09-07 06:21:27 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-	
+
 /**
  * main - Prints "with proper grammar, but the outcome is
  * a piece of art, followed by a new line."
  *
- * Return: Always 0.
- */
+ * Return: Always 0. 
+*/
 
 	int main(void)
 
