248fb5ba8e778e5ef1fd59672742785fe8354cba
kamau-marley <designsmarline@gmail.com>
2023-09-14 00:50:07 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/* main - prints the alpphabet in lowecase follwed by a new line
+ *
+ * returns (0)
+ */
+	void print_alphabet void 
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
